a44e67ddc7c21801c03fafed29f343e8d91cbb1b
phill <phillip26marchegiany@gmail.com>
2024-10-04 15:22:43 +0700

    login selesai, dan register selesai

--- lib/pages/authentication/register.dart
+++ lib/pages/authentication/register.dart
@@ -1,4 +1,7 @@
+import 'package:app_manajemen_umkm/pages/bottomNav.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_animate/flutter_animate.dart';
+import 'package:lottie/lottie.dart';
 
 class Register extends StatefulWidget {
   const Register({super.key});
@@ -8,12 +11,182 @@ class Register extends StatefulWidget {
 }
 
 class _RegisterState extends State<Register> {
+  bool _hintText = true;
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.amber,
+      resizeToAvoidBottomInset: true,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: LottieBuilder.asset(
+              "assets/lotties/backgroundLogin.json",
+              fit: BoxFit.cover, // memastikan background menutupi seluruh layar
+            ),
+          ),
+          Container(
+            alignment: Alignment.topCenter,
+            child: Positioned.fill(
+              child: LottieBuilder.asset(
+                "assets/lotties/backgroundLogin.json",
+                fit: BoxFit.fill,// memastikan background menutupi seluruh layar
+              ),
+            ),
+          ),
+          SingleChildScrollView(
+            child: Padding(
+              padding: EdgeInsets.only(top: 20, left: 60, right: 60),
+              child: Form(
+                  child: Column(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(top: 80),
+                    child: Container(
+                      alignment: Alignment.topCenter,
+                      child: LottieBuilder.asset(
+                        "assets/lotties/logoSignUp.json",
+                        width: 200,
+                      ),
+                    ),
+                  ),
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: 'Email',
+                      hintStyle: TextStyle(color: Colors.amber),
+                      labelStyle: TextStyle(color: Colors.amber),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1)),
+                      errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1)),
+                      focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1)),
+                      fillColor: Colors.white, // Warna background
+                      filled: true, // Mengaktifkan background
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    obscureText: _hintText,
+                    decoration: InputDecoration(
+                      hintText: 'Nama Toko',
+                      hintStyle: TextStyle(color: Colors.amber),
+                      labelStyle: TextStyle(color: Colors.amber),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.black, width: 1)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.black, width: 1)),
+                      errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.black, width: 1)),
+                      focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.black, width: 1)),
+                      fillColor: Colors.white, // Warna background
+                      filled: true, // Mengaktifkan background
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    obscureText: _hintText,
+                    decoration: InputDecoration(
+                      hintText: 'Password',
+                      hintStyle: TextStyle(color: Colors.amber),
+                      labelStyle: TextStyle(color: Colors.amber),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1)),
+                      errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1)),
+                      focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                              BorderSide(color: Colors.black, width: 1)),
+                      fillColor: Colors.white, // Warna background
+                      filled: true, // Mengaktifkan background
+                    ),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    obscureText: _hintText,
+                    decoration: InputDecoration(
+                      hintText: 'Konfirmasi Password',
+                      hintStyle: TextStyle(color: Colors.amber),
+                      labelStyle: TextStyle(color: Colors.amber),
+                      enabledBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.black, width: 1)),
+                      focusedBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.black, width: 1)),
+                      errorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.black, width: 1)),
+                      focusedErrorBorder: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(30),
+                          borderSide:
+                          BorderSide(color: Colors.black, width: 1)),
+                      fillColor: Colors.white, // Warna background
+                      filled: true, // Mengaktifkan background
+                    ),
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {
+                        Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => BottomNav()));
+                      },
+                      child: Text(
+                        "Sign up",
+                        style: TextStyle(color: Colors.amber),
+                      )),
+                  SizedBox(
+                    height: 10,
+                  ),
+                ],
+              )),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
 
-// ElevatedButton(onPressed: (){
-// Navigator.pop(context);
-// }, child: Text("Register"));
